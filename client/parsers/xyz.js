f7ab36bbdfece4fa0971d029f7eb96fa4e630ebd
Barry Moore <moore0557@gmail.com>
2015-11-15 20:15:18 -0500

    Update with File Parser and Contribution Etc.

--- client/parsers/xyz.js
+++ client/parsers/xyz.js
@@ -13,7 +13,6 @@ xyzParser = function (fileContents) {
     for (let i = 0; i < numParticles; i++) {
         coord = sp[i+2].split(/[ ]+/);
         object = AtomicData.findOne({symbol: coord[0].toLowerCase()});
-        console.log(object.color);
 
         floatVector = coord.slice(1).map(Number);
         vector = new THREE.Vector3(floatVector[0], floatVector[1], floatVector[2]);
