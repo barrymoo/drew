7b57a7bf3ee770a88526b93ab8c6404bd28c0b6a
Barry Moore II <moore0557@gmail.com>
2015-12-08 20:54:32 -0500

    Three.js BufferGeometry for Atoms and Bonds

--- client/parsers/master.js
+++ client/parsers/master.js
@@ -35,32 +35,133 @@ masterParser = function (file) {
  * @param {Object} parsedData A special data type, see {@link xyzParser}
  */
 parserSuccess = function (parsedData) {
-  var geometry = new THREE.BufferGeometry();
+  // Shaders and ShaderMaterial
+  var vertex_shader = "\
+    attribute float size;\
+    attribute vec3 color;\
+    varying vec3 vColor;\
+    \
+    void main() {\
+      vColor = color;\
+      vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);\
+      gl_PointSize = size * (450.0 / length(mvPosition.xyz));\
+      gl_Position = projectionMatrix * mvPosition;\
+    }\
+  ";
+
+  var fragment_shader = "\
+    varying vec3 vColor;\
+    \
+    void main() {\
+      if (length(gl_PointCoord * 2.0 - 1.0) > 1.0)\
+        discard;\
+        gl_FragColor = vec4(vColor, 1.0);\
+    }\
+  ";
+
+  var material = new THREE.ShaderMaterial({
+    vertexShader: vertex_shader,
+    fragmentShader: fragment_shader,
+    transparent: false
+  })
+
+  // Get the BufferGeometry attributes
   var length = parsedData.length;
   var positions = new Float32Array(length * 3);
+  var sizes = new Float32Array(length);
   var colors = new Float32Array(length * 3);
+  var color =  new THREE.Color();
+  for (var i = 0, i3 = 0; i < length; i++, i3 += 3) {
+    // set positions
+    positions[i3] = parsedData[i].vector.x;
+    positions[i3 + 1] = parsedData[i].vector.y;
+    positions[i3 + 2] = parsedData[i].vector.z;
 
-  var color = new THREE.Color();
+    // set sizes
+    sizes[i] = parsedData[i].atomicRadius;
 
-  var i = 0;
-  for (i; i < length * 3; i += 3) {
-    var index = Math.floor(i / 3);
-    positions[i] = parsedData[index].vector.x;
-    positions[i + 1] = parsedData[index].vector.y;
-    positions[i + 2] = parsedData[index].vector.z;
-
-    color.setHex(parsedData[index].color);
-    colors[i] = color.r;
-    colors[i + 1] = color.g;
-    colors[i + 2] = color.b;
+    // set colors
+    color.setHex(parsedData[i].color);
+    colors[i3] = color.r
+    colors[i3 + 1] = color.g
+    colors[i3 + 2] = color.b
   }
 
+  // Set BufferAttributes
+  var geometry = new THREE.BufferGeometry();
+  geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
+  geometry.addAttribute('size', new THREE.BufferAttribute(sizes, 1));
+  geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+  var particles = new THREE.Points(geometry, material);
+  scene.add(particles);
+  render();
+
+  // Generate Bond Pairs for use of Buffer Geometry
+  var bondsGeometry = new THREE.Geometry();
+  var distanceTo = 0, bondDistance = 0, pairs = [];
+  for (var i = 0; i < length; i++) {
+    for (var j = i + 1; j < length; j++) {
+      distanceTo = parsedData[i].vector.distanceTo(parsedData[j].vector);
+      bondDistance = parsedData[i].atomicRadius + parsedData[j].atomicRadius;
+      if (distanceTo <= bondDistance) {
+        pairs.push([i,j]);
+      }
+    }
+  }
+
+  // We'll reuse length, positions, colors, and sizes
+  // --> *4 because we want to use midpoints to separate bonds
+  length = pairs.length
+  positions = new Float32Array(length * 3 * 4);
+  colors = new Float32Array(length * 3 * 4);
+
+  for (var i=0, i4 = 0; i < length; i++, i4 += 12) {
+    // Compute Midpoint
+    var midPoint = new THREE.Vector3();
+    midPoint.subVectors(parsedData[pairs[i][1]].vector, parsedData[pairs[i][0]].vector);
+    midPoint.addVectors(parsedData[pairs[i][0]].vector, midPoint.multiplyScalar(0.5));
+
+    // Set positions
+    positions[i4 + 0] = parsedData[pairs[i][0]].vector.x;
+    positions[i4 + 1] = parsedData[pairs[i][0]].vector.y;
+    positions[i4 + 2] = parsedData[pairs[i][0]].vector.z;
+    positions[i4 + 3] = midPoint.x;
+    positions[i4 + 4] = midPoint.y;
+    positions[i4 + 5] = midPoint.z;
+    positions[i4 + 6] = midPoint.x;
+    positions[i4 + 7] = midPoint.y;
+    positions[i4 + 8] = midPoint.z;
+    positions[i4 + 9] = parsedData[pairs[i][1]].vector.x;
+    positions[i4 + 10] = parsedData[pairs[i][1]].vector.y;
+    positions[i4 + 11] = parsedData[pairs[i][1]].vector.z;
+
+    // Set Colors
+    color.setHex(parsedData[pairs[i][0]].color);
+    colors[i4 + 0] = color.r;
+    colors[i4 + 1] = color.g;
+    colors[i4 + 2] = color.b;
+    colors[i4 + 3] = color.r;
+    colors[i4 + 4] = color.g;
+    colors[i4 + 5] = color.b;
+
+    color.setHex(parsedData[pairs[i][1]].color);
+    colors[i4 + 6] = color.r;
+    colors[i4 + 7] = color.g;
+    colors[i4 + 8] = color.b;
+    colors[i4 + 9] = color.r;
+    colors[i4 + 10] = color.g;
+    colors[i4 + 11] = color.b;
+  }
+
+  // Reuse Geometry
+  geometry = new THREE.BufferGeometry();
   geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
   geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3));
 
-  var material = new THREE.PointsMaterial({vertexColors: THREE.VertexColors});
-  var particleSystem = new THREE.Points(geometry, material);
-  scene.add(particleSystem);
+  var bondMaterial = new THREE.LineBasicMaterial({linewidth: 5, vertexColors: THREE.VertexColors});
+  var bondLineSegments = new THREE.LineSegments(geometry, bondMaterial);
+  scene.add(bondLineSegments);
   render();
 };
 
